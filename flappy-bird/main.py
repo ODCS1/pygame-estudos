0b4295c6e30f08d7759972442114552d3e6a83c0
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-05-12 16:29:26 -0300

    Update main.py

--- flappy-bird/main.py
+++ flappy-bird/main.py
@@ -1,6 +1,6 @@
 import pygame as pg
 import os
-import random as r
+import random
 
 # Criação das constantes
 TELA_LARGURA = 500
@@ -67,7 +67,7 @@ class Passaro:
               if self.angulo > -90:
                   self.angulo -= self.VELOCIDADE_ROTACAO
 
-    def desenhar(self):
+    def desenhar(self, tela):
         # Definir qual imagem o passáro vai usar
         self.contagem_imagem += 1
 
@@ -84,15 +84,162 @@ class Passaro:
             self.contagem_imagem = 0
 
         # Se o passáro estiver caindo, não bate asa
+        if self.angulo <= -80:
+            self.imagem = self.IMGS[1]
+            self.angulo = -90
+            # ajuste
+            self.contagem_imagem = self.TEMPO_ANIMACAO * 2
 
+        # Desenhar a imagem
+        imagem_rotacionada = pg.transform.rotate(self.imagem, self.angulo)
+
+        posicao_centro_imagem = self.imagem.get_react(topleft=(self.x, self.y)).center
+        retangulo = imagem_rotacionada.get_rect(center = posicao_centro_imagem)
+
+        pg.tela.blit(imagem_rotacionada, retangulo.topleft)
          
-        
-
+    # Criar os pixels no retângulo do passáro
+    def get_mask(self):
+        return pg.mask.from_surface(self.imagem)
 
 
 
 class Cano:
-    pass
+    DISTANCIA = 200
+    VELOCIDADE = 5
+
+    def __init__(self, x):
+        self.x = x
+        self.pos_topo = 0
+        self.pos_base = 0
+        self.CANO_TOPO = pg.transform.flip(IMAGEM_CANO, False, True)
+        self.CANO_BASE = IMAGEM_CANO
+        self.passou = False
+        self.definir_altura()
+
+    def definir_altura(self):
+        self.altura = random.randrange(50, 450)
+        self.pos_topo = self.altura - self.CANO_TOPO.get_height
+        self.pos_base = self.altura + self.DISTANCIA
+
+    def mover(self):
+        self.x -= self.VELOCIDADE
+
+    def desenhar(self, tela):
+        tela.blit(self.CANO_TOPO, (self.x, self.pos_topo))
+        tela.blit(self.CANO_BASE, (self.x, self.pos_base))
+
+    def colidir(self, passsaro):
+        passsaro.mask = passsaro.get_mask()
+        topo_mask = pg.mask.from_surface(self.CANO_TOPO)
+        base_mask = pg.mask.from_surface(self.CANO_BASE)
+
+        distancia_topo = (self.x - passsaro.x, self.pos_topo - round(passsaro.y))
+        distancia_base = (self.x - passsaro.x, self.pos_base - round(passsaro.y))
+
+        topo_ponto = passsaro.mask.overlap(base_mask,distancia_topo)
+        base_ponto = passsaro.mask.overlap(base_mask,distancia_base)
+
+        # VERIFICAÇÃO
+        if base_ponto or topo_ponto:
+            return True
+        else:
+            return False
+
+
+
 
 class Chao:
-    pass
+    VELOCIDADE = 5
+    LARGURA = IMAGEM_CHAO.get_width()
+    IMAGEM = IMAGEM_CHAO
+
+    def __init__(self, y):
+        self.y = y
+        self.x1 = 0
+        self.x2 = self.LARGURA
+        
+    def mover(self):
+        self.x1 -= self.VELOCIDADE
+        self.x2 -= self.VELOCIDADE
+
+        if self.x1 + self.LARGURA < 0:
+            self.x1 =  self.x2 + self.LARGURA
+        if self.x2 + self.LARGURA < 0:
+            self.x2 = self.x1 + self.LARGURA
+
+    def desenhar(self, tela):
+        tela.blit(self.IMAGEM, (self.x1, self.y))
+        tela.blit(self.IMAGEM, (self.x2, self.y))
+ 
+def desenhar_tela(tela, passaros, canos, chao, pontos):
+    tela.blit(IMAGEM_FUNDO, (0, 0))
+    for passaro in passaros:
+        passaro.desenhar(tela)
+    for cano in canos:
+        cano.desenhar(tela)
+    
+    texto = FONTE_PONTOS.render(f'Pontuação: {pontos}', 1, (255, 255, 255))
+    tela.blit(texto, (TELA_LARGURA - 10 - texto.get_width(), 10))
+    chao.desenhar(tela)
+    pg.display.update()
+
+
+def main():
+    passaros = [Passaro(230, 350)]
+    chao = Chao(730)
+    canos = [Cano(700)]
+    tela = pg.display.set_mode(TELA_LARGURA, TELA_ALTURA)
+    pontos = 0
+    relogio = pg.time.Clock()
+
+    rodando = True
+    while rodando:
+        relogio.tick(30)
+
+        # Interação com o usuário
+        for evento in pg.event.get():
+            if evento.type == pg.QUIT:
+                rodando = False
+                pg.quit()
+                quit()
+            if evento.type == pg.KEYDOWN:
+                if evento.key == pg.K_SPACE:
+                    for passaro in passaros:
+                        passaro.pular()
+        # Mover as coisas
+        for passaro in passaros:
+            passaro.mover()
+        chao.mover()
+
+        adicionar_cano = False
+        remover_canos = []
+        for cano in canos:
+            for i, passaro in enumerate(passaros):
+                if cano.colidir(passaro):
+                    passaros.pop(i)
+                if not cano.passou and  passaro.x > cano.x:
+                    cano.passou = True
+                    adicionar_cano = True
+            cano.mover()
+            if cano.x + cano.CANO_TOPO.get_width() < 0:
+                remover_canos.append(cano)
+        
+        if adicionar_cano:
+            pontos += 1
+            canos.append(Cano(600))
+        for cano in remover_canos:
+            cano.remove(cano)
+
+        # Caso saia do eixo Y
+        for i, passaro in enumerate(passaros):
+            if  (passaro.y + passaro.imagem.get_height()) > chao.y or passaro.y < 0:
+                passaros.pop(i)
+
+
+        desenhar_tela(tela, passaros, canos, chao, pontos)
+
+
+
+if __name__ == '__main__':
+    main()
